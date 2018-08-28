3ad2204a1ee825270764d1066c81463b55953a0a
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 17:01:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,11 +9,13 @@ class Artist
   def initialize(name)
     @name = name 
     @songs = []
+    @songs_count = 0
   end 
   
   def add_song(song)
     @songs << song
     song.artist = self
+    @songs_count += 1
   end 
   
   def add_song_by_name(name)
