910c694ef1c0e122b971c8941e01df87cc206d07
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 16:34:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,19 +1,1 @@
 
-class Song 
-  
-  attr_accessor :name, :artist 
-  @@all []
-  
-  def self.all 
-    @@all   
-  end 
-  
-  def initialize(name)
-    @name = name
-    @@all << name 
-  end 
-  
-  def artist_name
-    self.artist.name
-  end
-end 
