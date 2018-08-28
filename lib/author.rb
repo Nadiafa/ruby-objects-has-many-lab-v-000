f2f6df7e2b5704ae040eb99a03e0c031007b6954
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-28 17:30:57 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,7 +7,32 @@ class Author
   end 
   
   def add_post(post)
-    @post = post
-    self.post << self
+    @posts = post
+    post.author = self
   end
 end 
+
+
+
+# class Artist 
+#   attr_accessor :name
+#   def self.song_count
+#     Song.all.count 
+#   end
+#   def initialize(name)
+#     @name = name 
+#     @songs = []
+#   end 
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end 
+#   def add_song_by_name(name)
+#     song = Song.new(name)
+#     @songs << song
+#     song.artist = self
+#   end 
+#   def songs
+#     @songs 
+#   end
+# end 
